dfd1f70a11c8015cb4b76e64eb069b18164248f7
Eric Noyau <noyau@google.com>
2019-09-02 18:40:31 +0200

    A mess I still need to cleanup

--- worldclock/worldclock.ino
+++ worldclock/worldclock.ino
@@ -10,6 +10,10 @@
 // with the various sensors.
 #define DEBUG 1
 #if defined(DEBUG)
+// Enable the blinking of the board light for debugging.
+#define DTICK 1
+
+// Same as Serial.print[ln] but shorter and removed from the code in non debug mode.
 #define DLOG(...) Serial.print(__VA_ARGS__)
 #define DLOGLN(...) Serial.println(__VA_ARGS__)
 #define DCHECK(condition, ...) \
@@ -24,7 +28,6 @@
 // To use the plotter to see variables, uncomment the one you want to see
 // #define PLOT_LDR 1
 // #define PLOT_BRIGHTNESS 1
-
 
 #define DLOG(...)
 #define DLOGLN(...)
@@ -52,7 +55,6 @@ void setupRTC() {
  * permanent little changes the reads are averaged over a long period, and the
  * change of brightness is animated to not be too jarring.
  */
-
 #define LDR_PIN 33
 
 class LDRReader {
@@ -93,6 +95,26 @@ class LDRReader {
  * constant (TopLeft, BottomLeft, BottomRight, TopRight), and for the matrix
  * use the topo macro, with the x/y position desired. (0,0) is top left.
  */
+
+// -----------------
+// The strip of leds
+// -----------------
+
+// With the light sensor on the bottom the panel is assumed to be wired as follow when viewed from
+// the front:
+
+// 02                                           01
+//   113 112 111 110 109 108 107 106 105 104 103
+//   092 093 094 095 096 097 098 099 100 101 102
+//   091 090 089 088 087 086 085 084 083 082 081
+//   070 071 072 073 074 075 076 077 078 079 080   
+//   069 068 067 066 065 064 063 062 061 060 059
+//   048 049 050 051 052 053 054 055 056 057 058
+//   047 046 045 044 043 042 041 040 039 038 037 
+//   026 027 028 029 030 031 032 033 034 035 036
+//   025 024 023 022 021 020 019 018 017 016 015
+//   004 005 006 007 008 009 010 011 012 013 014
+// 03                                           00
 
 // Defines how the matrix is laid out, and how to access the pixels.
 #define NEOPIXEL_PIN       32
@@ -146,168 +168,193 @@ enum Signals {
  */
 NeoPixelAnimator animations(NEOPIXEL_COUNT, NEO_CENTISECONDS);
 
-
 /*
  * Constants to match the face.
  *
  * Custom French face. Can show time with some less usual variations like
    MINUIT TROIS QUARTS or DEUX HEURES PILE. This also includes all the letters
-   of the alphabets as well as !. Letters in lowercase below are not used
-   by the clock.
+   of the alphabets as well as !.
+  
+   The first table is the one in the code, with 8 available spaces. Those are filled with letters to
+   have the full alphabet on the board, to eventually use as a ouija board (the missing letters are
+   bjkwy, with the punctuation @!? for good measure)
 
-  LOl, il manque le DIX!
+   The next three are optimized to open more space by grouping words.
 
-  ILbESTjDEUX
-  QUATRETROIS
-  NEUFUNESEPT
-  HUITSIXCINQ
-  MIDIXMINUIT
-  ONZEwHEURES
-  MOINSyLEDIX
-  ETTROISDEMI
-  VINGT-CINQk
-  QUARTSPILE!
+   The second offers the same functionality but opens 12 open spaces, plus the possibility of two
+   additional three letter words (Vertically U...O, horizontally S...D).
+
+   The third limits the use of "PILE" (which is not a huge loss) to open 2 more spots, for a total
+   of 14 available spaces, and this one can spell four words of two, three and four characters
+   (Vertically U...O & S...., horizontally S..D, S.., with additional two 2 letters word possible
+   on line 3 and 4).
+
+   The last one is optimized the fullest, removing the second "TROIS" and the "PILE". It uses only
+   9 lines, leaving space for 11 char customization and up to two 2 letter words, and a 3 vertical.
+
+  ILbESTjDEUX  IL.EST.DEUX  IL.EST.DEUX  IL.EST.DEUX
+  QUATRETROIS  UNEUFMINUIT  UNEUFMINUIT  UNEUFMINUIT
+  @kUNEUFSEPT  .SEPTROISIX  .MIDIX.HUIT  ..MIDIXHUIT
+  HUITSIXCINQ  .CINQUATRE.  .CINQUATRE.  ..CINQUATRE
+  MIDIXMINUIT  .MIDIX.HUIT  .SEPTROISIX  .SEPTROISIX
+  ONZEwHEURES  ONZE.HEURES  ONZE.HEURES  ONZE.HEURES
+  MOINSyLEDIX  MOINS...DIX  MOINS..DIX.  .MOINSETDIX
+  ETTROISDEMI  LETROISDEMI  PILETROIS..  .VINGT-CINQ
+  VINGT-CINQ?  VINGT-CINQ.  VINGT-CINQ.  LEDEMIQUART
+  QUARTSPILE!  QUARTSPILE.  DEMIQUARTS.  ...........
 */
 
-// All the segments of words on the board. The first too numbers are the
-// coordinate of the first letter of the word, the last is the length. A word
-// must always be on one row.
-#define S_IL         0,0, 2
-#define S_EST        3,0, 3
+template <typename W> class ClockFace {
+protected: 
+  typedef struct { int x, y, length; W word; } Segment;
 
-#define H_UNE        4,2, 3
-#define H_DEUX       7,0, 4
-#define H_TROIS      6,1, 5
-#define H_QUATRE     0,1, 6
-#define H_CINQ       7,3, 4
-#define H_SIX        4,3, 3
-#define H_SEPT       7,2, 4
-#define H_HUIT       0,3, 4
-#define H_NEUF       0,2, 4
-#define H_DIX        2,4, 3
-#define H_ONZE       0,5, 4
-                    
-#define H_HEURE      5,5, 5
-#define H_HEURES     5,5, 6
-                    
-#define H_MIDI       0,4, 4
-#define H_MINUIT     5,4, 6
-                    
-#define M_MOINS      0,6, 5
-#define M_LE         6,6, 2
-#define M_ET         0,7, 2
-#define M_TROIS      2,7, 5
+  // Lit a segment in updateState.
+  void updateSegment(W word) {
+    Segment segment = _segments[(int)word];
+    DCHECK(word == segment.word, word); 
+    for (int i = segment.x; i <= segment.x + segment.length - 1; i++)
+      _updateState[topo(i,segment.y)] = true;
+  }
 
-#define M_DIX        8,6, 3
-#define M_QUART      3,7, 5
-#define M_VINGT      0,8, 5
-#define M_VINGTCINQ  0,8,10
-#define M_CINQ       6,8, 4
-#define M_DEMI       7,7, 4
-#define M_QUART      0,9, 5
-#define M_QUARTS     0,9, 6
-#define M_PILE       6,9, 4
+  bool _updateState[NEOPIXEL_COUNT];
 
-// Stores the bits of the clock that need to be turned on.
-bool updateState[NEOPIXEL_COUNT];
-
-// Lit a segment in updateState.
-void updateSegment(int x, int y, int length) {
-  for (int i = x; i <= x + length - 1; i++)
-    updateState[topo(i,y)] = true;
-}
-
-// From an hour and a minute updates the updateState variable to display that
-// time on the clock.
-void timeDecoder(int hours, int minutes) {
-  int leftover = minutes % 5;
-  minutes = minutes - leftover;
-
-  if (minutes >= 35)
-    hours += 1;  // Switch to "TO" minutes the next hour
+private:
+  // Stores the bits of the clock that need to be turned on.
+  Segment _segments[];
   
-  updateSegment(S_IL);
-  updateSegment(S_EST);
+public:
+  virtual void timeDecoder(int hours, int minutes) = 0;
+};
 
-  switch (hours) {
-    case  0:
-      updateSegment(H_MINUIT); break;
-    case  1: case 13:
-      updateSegment(H_UNE); break;
-    case  2: case 14:
-      updateSegment(H_DEUX); break;
-    case  3: case 15:
-      updateSegment(H_TROIS); break;
-    case  4: case 16:
-      updateSegment(H_QUATRE); break;
-    case  5: case 17:
-      updateSegment(H_CINQ); break;
-    case  6: case 18:
-      updateSegment(H_SIX); break;
-    case  7: case 19:
-      updateSegment(H_SEPT); break;
-    case  8: case 20:
-      updateSegment(H_HUIT); break;
-    case  9: case 21:
-      updateSegment(H_NEUF); break;
-    case 10: case 22:
-      updateSegment(H_DIX); break;
-    case 11: case 23:
-      updateSegment(H_ONZE); break;
-    case 12: 
-      updateSegment(H_MIDI); break;
-  }
-  switch (hours) {
-    case  0: case 12:
-      break;
-    case  1: case 13:
-      updateSegment(H_HEURE); break;
-    default:
-      updateSegment(H_HEURES); break;
-  }
+// All the segments representing words on the board in order of appearance
+typedef enum {
+  IL, EST, DEUX,
+  QUATRE, TROIS,
+  UNE, NEUF, SEPT,
+  HUIT, SIX, CINQ, 
+  MIDI, DIX, MINUIT,
+  ONZE, HEURE, HEURES, 
+  MOINS, LE, M_DIX,
+  ET, M_TROIS, DEMI,
+  VINGT, VINGTCINQ, M_CINQ, 
+  QUART, QUARTS, PILE
+} FrenchWords; 
 
-  switch (minutes) {
-    case 0:
-      break;
-    case 5:
-      updateSegment(M_CINQ); break;
-    case 10:
-      updateSegment(M_DIX); break;
-    case 15:
-      updateSegment(M_ET); updateSegment(M_QUART); break;
-    case 20:
-      updateSegment(M_VINGT); break;
-    case 25:
-      updateSegment(M_VINGTCINQ); break;
-    case 30:
-      updateSegment(M_ET); updateSegment(M_DEMI); break;
-    case 35:
-      updateSegment(M_MOINS); updateSegment(M_VINGTCINQ); break;
-    case 40:
-      updateSegment(M_MOINS); updateSegment(M_VINGT); break;
-    case 45:
-      updateSegment(M_MOINS); updateSegment(M_LE); updateSegment(M_QUART); break;
-    case 50:
-      updateSegment(M_MOINS); updateSegment(M_DIX); break;
-    case 55:
-      updateSegment(M_MOINS); updateSegment(M_CINQ); break;
-    default:
-      updateSegment(10, 9, 1);
-  }
+
+
+class FrenchClockFace : ClockFace<FrenchWords> {
+  // The first too numbers are the coordinate of the first letter of the word, the third is the
+  // length. A word must always be on one row. The last item is only used to validate the table is
+  // well formed.
+  ClockFace<FrenchWords>::Segment _segments[] = {
+    { 0,0, 2, IL },     { 3,0, 3, EST },       { 7,0, 4, DEUX },
+    { 0,1, 6, QUATRE }, { 6,1, 5, TROIS },
+    { 2,2, 3, UNE },    { 3,2, 4, NEUF },      { 7,2, 4, SEPT },
+    { 0,3, 4, HUIT },   { 4,3, 3, SIX },       { 7,3, 4, CINQ },
+    { 0,4, 4, MIDI },   { 2,4, 3, DIX },       { 5,4, 6, MINUIT },
+    { 0,5, 4, ONZE },   { 5,5, 5, HEURE },     { 5,5, 6, HEURES },
+    { 0,6, 5, MOINS },  { 6,6, 2, LE },        { 8,6, 3, M_DIX },
+    { 0,7, 2, ET },     { 2,7, 5, M_TROIS },   { 7,7, 4, DEMI },
+    { 0,8, 5, VINGT },  { 0,8,10, VINGTCINQ }, { 6,8, 4, M_CINQ },
+    { 0,9, 5, QUART },  { 0,9, 6, QUARTS },    { 6,9, 4, PILE },
+  };
+  // From an hour and a minute updates the updateState variable to display that
+  // time on the clock.
+  virtual void timeDecoder(int hours, int minutes) {
+    int leftover = minutes % 5;
+    minutes = minutes - leftover;
+
+    if (minutes >= 35)
+      hours += 1;  // Switch to "TO" minutes the next hour
   
-  switch (leftover) {
-    case 4:
-      updateState[TopLeft] = true;
-    case 3:
-      updateState[BottomLeft] = true;
-    case 2:
-      updateState[BottomRight] = true;
-    case 1:
-      updateState[TopRight] = true;
-    case 0:
-      break;
+    updateSegment(IL);
+    updateSegment(EST);
+
+    switch (hours) {
+      case  0:
+        updateSegment(MINUIT); break;
+      case  1: case 13:
+        updateSegment(UNE); break;
+      case  2: case 14:
+        updateSegment(DEUX); break;
+      case  3: case 15:
+        updateSegment(TROIS); break;
+      case  4: case 16:
+        updateSegment(QUATRE); break;
+      case  5: case 17:
+        updateSegment(CINQ); break;
+      case  6: case 18:
+        updateSegment(SIX); break;
+      case  7: case 19:
+        updateSegment(SEPT); break;
+      case  8: case 20:
+        updateSegment(HUIT); break;
+      case  9: case 21:
+        updateSegment(NEUF); break;
+      case 10: case 22:
+        updateSegment(DIX); break;
+      case 11: case 23:
+        updateSegment(ONZE); break;
+      case 12: 
+        updateSegment(MIDI); break;
+      default:
+        DCHECK(!hours, hours);
+    }
+    switch (hours) {
+      case  0: case 12:
+        break;
+      case  1: case 13:
+        updateSegment(HEURE); break;
+      default:
+        updateSegment(HEURES); break;
+    }
+
+    switch (minutes) {
+      case 0:
+        break;
+      case 5:
+        updateSegment(M_CINQ); break;
+      case 10:
+        updateSegment(M_DIX); break;
+      case 15:
+        updateSegment(ET); updateSegment(QUART); break;
+      case 20:
+        updateSegment(VINGT); break;
+      case 25:
+        updateSegment(VINGTCINQ); break;
+      case 30:
+        updateSegment(ET); updateSegment(DEMI); break;
+      case 35:
+        updateSegment(MOINS); updateSegment(VINGTCINQ); break;
+      case 40:
+        updateSegment(MOINS); updateSegment(VINGT); break;
+      case 45:
+        updateSegment(MOINS); updateSegment(LE); updateSegment(QUART); break;
+      case 50:
+        updateSegment(MOINS); updateSegment(M_DIX); break;
+      case 55:
+        updateSegment(MOINS); updateSegment(M_CINQ); break;
+      default:
+        DCHECK(!minutes, minutes);
+    }
+  
+    switch (leftover) {
+      case 4:
+        _updateState[TopLeft] = true;
+      case 3:
+        _updateState[BottomLeft] = true;
+      case 2:
+        _updateState[BottomRight] = true;
+      case 1:
+        _updateState[TopRight] = true;
+      case 0:
+        break;
+    }
   }
-}
+
+};
+ 
+
 
 #define TIME_CHANGE_ANIMATION_SPEED 400
 
@@ -478,6 +525,14 @@ class BrightnessController {
 
 BrightnessController brightnessController;
 
+#if defined(DTICK) 
+void blinkTime() {
+  bool secondBit = rtc.now().unixtime() & 1;
+  int ledValue = _lightSensor.value() * (secondBit ? 2048 : 4095);
+  ledcWrite(LEDC_CHANNEL, ledValue);
+}
+#endif DTICK
+
 void setup() {
   Serial.begin(115200);
   
@@ -513,4 +568,10 @@ void loop() {
   brightnessController.update();
   animations.UpdateAnimations();
   pixels.Show();
+
+  // Tictoc
+#if defined(DTICK) 
+  blinkTime();
+#endif
+
 }
